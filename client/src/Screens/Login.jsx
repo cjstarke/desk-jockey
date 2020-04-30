cea28bb809e5a6124548f3379559609123d09689
Christopher Starke <CJstarke@Christophers-MacBook-Air.local>
2020-04-30 18:44:23 -0400

    error handling on signup and login

--- client/src/Screens/Login.jsx
+++ client/src/Screens/Login.jsx
@@ -4,13 +4,39 @@ import { Link } from 'react-router-dom';
 
 
 // This component handles our login form and has a link to the register form
-const Login = (props) => {
+class Login extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      loading: false,
+      error: false
+    }
+  }
 
-  const Submit = async (e) => {
+  Submit = async (e) => {
     e.preventDefault();
-    await props.handleLogin().then(()=> props.history.push(`/home`))
+    this.setState({
+      loading: true
+    })
+    await this.props.handleLogin().then(()=>
+     {this.props.history.push(`/home`)},()=>{
+       this.setState({
+         loading: false,
+         error : true
+       })
+     })
 
   }
+
+render () {
+let fill = (<div className="LoginSlogan">All the worlds a stage, and all your drab office is a drum kit</div>)
+ if(this.state.loading) {
+   fill = ( <div className="lds-ring"><div></div><div></div><div></div><div></div></div>)
+ }
+ else if (this.state.error) {
+   fill = (<div className="LoginSLogan">Sorry, try again</div>)
+ }
+
 
   return (
     <div className= "LoginPage">
@@ -24,24 +50,26 @@ const Login = (props) => {
       </div>
       <div className="LoginProcess">
         <div>Login</div>
-        <form className="LoginForm" onSubmit={Submit} >
+        <form className="LoginForm" onSubmit={this.Submit} >
           <div className="LoginField">
             <label>Username:</label>
-            <input name="username" type="text" value={props.formData.username} onChange={props.handleChange} />
+            <input name="username" type="text" value={this.props.formData.username} onChange={this.props.handleChange} />
           </div>
           <div className="LoginField">
             <label>Password:</label>
-            <input name="password" type="password" value={props.formData.password} onChange={props.handleChange} />
+            <input name="password" type="password" value={this.props.formData.password} onChange={this.props.handleChange} />
           </div>
           
           <button className="LoginButton">Login</button>
           <span>or  <Link to="/register">Sign up</Link></span>
         </form>
-        <div className="LoginSlogan">All the worlds a stage, and all your drab office is a drum kit</div>
+        {fill}
       </div>
      
     </div>
   );
+}
+
 }
 
 export default Login;
--- client/src/Screens/Register.jsx
+++ client/src/Screens/Register.jsx
@@ -3,40 +3,70 @@ import deskjockeylogo from '../pictures/deskjockeylogo.png'
 import deskworker from '../pictures/deskworker.png'
 
 // This component handles our register form
-const Register = (props) => {
-  const Submit = async (e) => {
-    e.preventDefault();
-    await props.handleRegister().then(()=> props.history.push(`/home`))
+class Register extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      loading: false,
+      error: false
+    }
   }
+
+  Submit = async (e) => {
+    e.preventDefault();
+    this.setState({
+      loading: true
+    })
+    await this.props.handleRegister().then(()=>
+     {this.props.history.push(`/home`)},()=>{
+       this.setState({
+         loading: false,
+         error : true
+       })
+     })
+
+  }
+
+render () {
+let fill = (<div className="LoginSlogan">All the worlds a stage, and all your drab office is a drum kit</div>)
+ if(this.state.loading) {
+   fill = ( <div className="lds-ring"><div></div><div></div><div></div><div></div></div>)
+ }
+ else if (this.state.error) {
+   fill = (<div className="LoginSLogan">Sorry, try again</div>)
+ }
+
 
   return (
     <div className= "LoginPage">
-       <div className = "LoginImages">
+      <div className = "LoginImages">
         <div className= "LoginLogo">
-          <img src={deskjockeylogo} alt="logo"/>
+          <img src="https://i.imgur.com/kwvubEP.png" alt="logo"/>
         </div>
         <div className="LoginDesk">
-          <img src={deskworker} alt="desk"/>
+          <img src="https://i.imgur.com/QH3ZKQi.png" alt="desk"/>
         </div>
       </div>
       <div className="LoginProcess">
         <div>Sign Up</div>
-        <form onSubmit={Submit} className="LoginForm">
+        <form className="LoginForm" onSubmit={this.Submit} >
           <div className="LoginField">
-          <label>Username:</label>
-          <input name="username" type="text" value={props.formData.username} onChange={props.handleChange} />
+            <label>Username:</label>
+            <input name="username" type="text" value={this.props.formData.username} onChange={this.props.handleChange} />
           </div>
           <div className="LoginField">
-            <label className="LoginField">Password:</label>
-            <input name="password" type="password" value={props.formData.password} onChange={props.handleChange} />
+            <label>Password:</label>
+            <input name="password" type="password" value={this.props.formData.password} onChange={this.props.handleChange} />
           </div>
+          
           <button className="LoginButton">Sign Up</button>
+          
         </form>
-        <div className="LoginSlogan">All the worlds a stage, and all your drab office is a drum kit</div>
+        {fill}
       </div>
-
+     
     </div>
   );
 }
-
+}
 export default Register;
